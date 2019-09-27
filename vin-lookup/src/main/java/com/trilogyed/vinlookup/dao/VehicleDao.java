de5585aeb81aa942664d241dda4f8242cf8fd2de
terrenceturner <tsturner24@gmail.com>
2019-09-27 11:49:35 -0400

    fixed VehicleDao

--- vin-lookup/src/main/java/com/trilogyed/vinlookup/dao/VehicleDao.java
+++ vin-lookup/src/main/java/com/trilogyed/vinlookup/dao/VehicleDao.java
@@ -1,4 +1,9 @@
 package com.trilogyed.vinlookup.dao;
 
+import com.trilogyed.vinlookup.model.Vehicle;
+
 public interface VehicleDao {
+
+    public Vehicle getVehicleByVin(String vin);
+
 }
--- vin-lookup/src/main/java/com/trilogyed/vinlookup/dao/VehicleDaoJdbcTemplateImpl.java
+++ vin-lookup/src/main/java/com/trilogyed/vinlookup/dao/VehicleDaoJdbcTemplateImpl.java
@@ -1,4 +1,43 @@
 package com.trilogyed.vinlookup.dao;
 
-public class VehicleDaoJdbcTemplateImpl {
+import com.trilogyed.vinlookup.model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class VehicleDaoJdbcTemplateImpl implements VehicleDao {
+
+    //Prepared Statements
+    private static final String SELECT_VEHICLE_BY_VIN_SQL =
+            "select * from motorcycle where vin = ?";
+
+
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public VehicleDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public Vehicle getVehicleByVin(String vin) {
+        return jdbcTemplate.queryForObject(SELECT_VEHICLE_BY_VIN_SQL, this::mapRowToVehicle, vin);
+    }
+
+    //RowMapper
+    private Vehicle mapRowToVehicle(ResultSet rs, int rowNum) throws SQLException {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setType("Motorcycle");
+        vehicle.setMake(rs.getString("make"));
+        vehicle.setModel(rs.getString("model"));
+        vehicle.setYear(rs.getString("year"));
+        vehicle.setColor(rs.getString("color"));
+
+        return vehicle;
+    }
+
 }
